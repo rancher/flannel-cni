f1180f99ac9cc741cced09547f0627c268695e45
jianghang8421 <jianghang8421@163.com>
2018-10-15 17:32:17 +0800

    Add multiarch support

--- scripts/build-image.sh
+++ scripts/build-image.sh
@@ -2,13 +2,41 @@
 set -e
 CURL="curl -sSf"
 FLANNEL_CNI_ROOT=$(git rev-parse --show-toplevel)
-IMAGE_NAME=quay.io/coreos/flannel-cni
+IMAGE_NAME=rancher/flannel-cni
 VERSION=$($FLANNEL_CNI_ROOT/scripts/git-version)
 CNI_VERSION="v0.6.0"
 
-mkdir -p dist
-$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
-$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-amd64-$CNI_VERSION.tgz | tar -xz -C dist/
+ARCH=amd64
 
-docker build --no-cache -t $IMAGE_NAME:$VERSION .
-docker push $IMAGE_NAME:$VERSION
+case "$(uname -m)" in
+  x86_64*)
+    ARCH=amd64
+    ;;
+  i?86_64*)
+    ARCH=amd64
+    ;;
+  amd64*)
+    ARCH=amd64
+    ;;
+  aarch64*)
+    ARCH=arm64
+    ;;
+  arm64*)
+    ARCH=arm64
+    ;;
+  *)
+    echo "Unsupported host arch. Must be x86_64, arm64." >&2
+    exit 1
+    ;;
+esac
+
+mkdir -p dist
+$CURL -L --retry 5 https://github.com/containernetworking/cni/releases/download/$CNI_VERSION/cni-${ARCH}-$CNI_VERSION.tgz | tar -xz -C dist/
+$CURL -L --retry 5 https://github.com/containernetworking/plugins/releases/download/$CNI_VERSION/cni-plugins-${ARCH}-$CNI_VERSION.tgz | tar -xz -C dist/
+
+case ${ARCH} in
+    amd64) BASEIMAGE_ARCH="amd64" ;;
+    arm64) BASEIMAGE_ARCH="arm64v8" ;;
+esac
+sed -i "s|BASEIMAGE_ARCH|$BASEIMAGE_ARCH|g" Dockerfile
+docker build --no-cache -t $IMAGE_NAME:$VERSION-${ARCH} .
